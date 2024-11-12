7ffe058a5ca3cd2464515818b41d1cfb9c44371c
Tomaciej73 <92037535+Tomaciej73@users.noreply.github.com>
2024-11-12 08:38:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,74 +1,138 @@
 '''Zadanie 1'''
-cities = ['Warsaw', 'Krakow', 'Gdansk']
-it = iter(cities)
-while True:
-    try:
-        print(next(it))
-    except StopIteration:
-        break
+import time
+
+def dekorator(funkcja):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        wynik = funkcja(*args, **kwargs)
+        end = time.time()
+        print(f'Czas wykonania: {end - start} sekund')
+        return wynik
+    return wrapper
+
+@dekorator
+def metoda1():
+    for i in range(1000000):
+        pass
+
+@dekorator
+def metoda2():
+    for i in range(1000000):
+        print(i)
+
+metoda1()
+metoda2()
 
 '''Zadanie 2'''
-import sys
-n = int(sys.argv[1])
-print(sum(range(1, n+1)))
+class IteratorLiczb:
+    def __init__(self, limit):
+        self.limit = limit
+        self.current = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current <= self.limit:
+            liczba = self.current
+            self.current += 1
+            return liczba
+        else:
+            raise StopIteration
+
+iterator = IteratorLiczb(5)
+
+for liczba in iterator:
+    print(liczba)
 
 '''Zadanie 3'''
-n = int(sys.argv[1])
-print(sum(range(1, n+1)))
+class IteratorLiczb:
+    def __init__(self, limit):
+        self.limit = limit
+        self.current = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current <= self.limit:
+            liczba = self.current
+            self.current += 1
+            return liczba
+        else:
+            raise StopIteration
+
+iterator = IteratorLiczb(100)
+
+for liczba in iterator:
+    print(liczba)
 
 '''Zadanie 4'''
-celsius = [0, 20, 30]
-fahrenheit = list(map(lambda c: c * 9/5 + 32, celsius))
-print(fahrenheit)
+def przechwytuj_zerowe(funkcja):
+    def wrapper(self, liczba):
+        if liczba == 0:
+            print("Nie można dodać zera do kolekcji.")
+        else:
+            funkcja(self, liczba)
+    return wrapper
+
+class KolekcjaLiczb:
+    def __init__(self):
+        self.liczby = []
+
+    @przechwytuj_zerowe
+    def dodaj(self, liczba):
+        self.liczby.append(liczba)
+
+    def __iter__(self):
+        return (liczba for liczba in self.liczby if liczba > 0)
+
+kolekcja = KolekcjaLiczb()
+
+kolekcja.dodaj(5)
+kolekcja.dodaj(0) #Nie doda tego ez
+kolekcja.dodaj(-3)
+kolekcja.dodaj(7)
+
+for liczba in kolekcja:
+    print(liczba)
 
 '''Zadanie 5'''
-fahrenheit = [32, 68, 86]
-celsius = list(map(lambda f: (f - 32) * 5/9, fahrenheit))
-print(celsius)
+import random
+from abc import ABC, abstractmethod
+
+class ZestawDanych(ABC):
+    @abstractmethod
+    def generuj_dane(self):
+        pass
+
+class DaneLosowe(ZestawDanych):
+    def __init__(self, liczba_danych, min_wartosc=0, max_wartosc=100):
+        self.liczba_danych = liczba_danych
+        self.min_wartosc = min_wartosc
+        self.max_wartosc = max_wartosc
+
+    def generuj_dane(self):
+        for _ in range(self.liczba_danych):
+            yield random.randint(self.min_wartosc, self.max_wartosc)
+
+dane_losowe = DaneLosowe(10, 1, 50)
+
+for liczba in dane_losowe.generuj_dane():
+    print(liczba)
 
 '''Zadanie 6'''
-from itertools import islice
+class Fibonacci:
+    def __init__(self, n):
+        self.n = n
 
-def fibonacci(n):
-    a, b = 0, 1
-    for _ in range(n):
-        yield a
-        a, b = b, a + b
+    def __iter__(self):
+        a, b = 0, 1
+        for _ in range(self.n):
+            yield a
+            a, b = b, a + b
 
-print(list(filter(lambda x: x % 2 == 1, fibonacci(10))))
+fibonacci = Fibonacci(10)
 
-'''Zadanie 7'''
-from itertools import islice
-
-def fibonacci(n):
-    a, b = 0, 1
-    for _ in range(n):
-        yield a
-        a, b = b, a + b
-
-print(list(filter(lambda x: x % 2 == 1, fibonacci(10))))
-
-'''Zadanie 8'''
-from itertools import islice
-
-def fibonacci(n):
-    a, b = 0, 1
-    for _ in range(n):
-        yield a
-        a, b = b, a + b
-
-print(list(filter(lambda x: x % 2 == 1, fibonacci(10))))
-
-'''Zadanie 9'''
-from functools import reduce
-
-numbers = [3, 5, 7, 2, 8]
-largest = reduce(lambda a, b: a if a > b else b, numbers)
-print(largest)
-
-'''Zadanie 10'''
-from functools import reduce
-
-numbers = [3, 5, 7, 2, 8]
-largest = reduce(max, numbers)
-print(largest)
+for liczba in fibonacci:
+    print(liczba)
